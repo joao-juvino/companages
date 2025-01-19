80400c78f13dfcc478f68b67ad44d2e172e61200
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-01-18 23:17:37 -0300

    feat: adding user table

---
+++ src/main/java/com/companeges/demo/models/User.java
@@ -0,0 +1,48 @@
+package com.companeges.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users") 
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Auto-incremento
+    private Long id;
+    
+    @Column(nullable = false, length = 50) // Define as características da coluna
+    private String name;
+    
+    @Column(nullable = false, unique = true) // Define como única
+    private String email;
+    
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
