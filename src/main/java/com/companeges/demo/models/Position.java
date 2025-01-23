c3c97a9e2dffcc4bde5bc6ca2f3ad83556b59499
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-01-23 18:07:27 -0300

    feat: adding Position table

---
+++ src/main/java/com/companeges/demo/models/Position.java
@@ -0,0 +1,33 @@
+package com.companeges.demo.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cargo")
+public class Position {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column()
+    private Long weight;
+
+    @ManyToOne
+    @JoinColumn(name = "organization_id", nullable = false)
+    private Organization organization;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+}
