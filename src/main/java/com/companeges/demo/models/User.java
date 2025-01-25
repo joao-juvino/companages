ff476e27d134775cda8a1ae0590ebc2993aa38df
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-01-24 22:53:42 -0300

    feat: adding all gets and sets to User model

--- src/main/java/com/companeges/demo/models/User.java
+++ src/main/java/com/companeges/demo/models/User.java
@@ -39,6 +39,14 @@ public class User {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
     
     public String getName() {
         return name;
@@ -54,5 +62,14 @@ public class User {
     
     public void setEmail(String email) {
         this.email = email;
+    }
+
+
+    public List<Organization> getOrganizations() {
+        return organizations;
+    }
+    
+    public void setOrganizations(List<Organization> organizations) {
+        this.organizations = organizations;
     }
 }
