a74a8d63b4d08c322e097fd5760f0be5ef22c9fd
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-01-26 16:55:10 -0300

    feat: adding reference to user repository in service

--- src/main/java/com/companeges/demo/services/UserServiceImpl.java
+++ src/main/java/com/companeges/demo/services/UserServiceImpl.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.companeges.demo.dto.UserPostPutRequestDTO;
 import com.companeges.demo.models.User;
+import com.companeges.demo.repositories.UserRepository;
 
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
 
     @Autowired
     ModelMapper modelMapper;
@@ -14,7 +18,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserPostPutRequestDTO createUser(UserPostPutRequestDTO userPostPutRequestDTO) {
         User user = modelMapper.map(userPostPutRequestDTO, User.class);
-        animalsRepository.save(user);
+        userRepository.save(user);
         return modelMapper.map(user, UserPostPutRequestDTO.class);
     }
     
