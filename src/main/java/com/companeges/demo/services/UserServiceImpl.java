65bbcca2b71566e497b98784034e9d3a75c40b99
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-01-27 19:23:41 -0300

    fix: adding service annotation to user service

--- src/main/java/com/companeges/demo/services/UserServiceImpl.java
+++ src/main/java/com/companeges/demo/services/UserServiceImpl.java
@@ -2,11 +2,13 @@ package com.companeges.demo.services;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.companeges.demo.dto.UserPostPutRequestDTO;
 import com.companeges.demo.models.User;
 import com.companeges.demo.repositories.UserRepository;
 
+@Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
