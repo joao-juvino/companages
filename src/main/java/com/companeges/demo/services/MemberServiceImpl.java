9b6f80701251d058eff81bcc67a8393f49fe5ca7
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-01-31 16:35:58 -0300

    feat: adding method to create a new member in member service

--- src/main/java/com/companeges/demo/services/MemberServiceImpl.java
+++ src/main/java/com/companeges/demo/services/MemberServiceImpl.java
@@ -1,5 +1,27 @@
 package com.companeges.demo.services;
 
+import java.lang.reflect.Member;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.companeges.demo.dto.MemberPostPutDTO;
+import com.companeges.demo.repositories.MemberRepository;
+
+@Service
 public class MemberServiceImpl {
-    
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public MemberPostPutDTO createMember(MemberPostPutDTO memberPostPutDTO) {
+        Member member = modelMapper.map(memberPostPutDTO, Member.class);
+
+        memberRepository.save(member);
+
+        return modelMapper.map(member, MemberPostPutDTO.class);
+    }
 }
