df282855e7f0a6a543be3524a1122b3892a82952
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-02-01 14:59:19 -0300

    fix: adding sprint annotations to organization controller

--- src/main/java/com/companeges/demo/controllers/OrganizationController.java
+++ src/main/java/com/companeges/demo/controllers/OrganizationController.java
@@ -3,12 +3,16 @@ package com.companeges.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.companeges.demo.dto.OrganizationPostPutRequestDTO;
 import com.companeges.demo.services.OrganizationService;
 
+@Controller
+@RequestMapping("/organizations")
 public class OrganizationController {
     @Autowired
     OrganizationService organizationService;
