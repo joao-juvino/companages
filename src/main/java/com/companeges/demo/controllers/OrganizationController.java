cddf9431200dff626d62daf8353ad01963c06c90
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-02-06 21:36:11 -0300

    fix: adjusting annotation for mapping the route

--- src/main/java/com/companeges/demo/controllers/OrganizationController.java
+++ src/main/java/com/companeges/demo/controllers/OrganizationController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class OrganizationController {
                 .body(organizationService.createOrganization(organizationPostPutRequestDTO));
     } 
     
-    @PostMapping
+    @GetMapping
     public ResponseEntity<?> getOrganizations() {
         return ResponseEntity
                 .status(HttpStatus.OK)
