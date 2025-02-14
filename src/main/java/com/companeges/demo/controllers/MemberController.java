15b0a291d47c54d4cda07f7447c2ccc3b3539ca5
João Pedro Juvino dos Santos <joao.pedro.santos@ccc.ufcg.edu.br>
2025-02-14 19:56:16 -0300

    fix: adjusting the name of the member routers

--- src/main/java/com/companeges/demo/controllers/MemberController.java
+++ src/main/java/com/companeges/demo/controllers/MemberController.java
@@ -4,13 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.companeges.demo.dto.MemberPostPutDTO;
 import com.companeges.demo.services.MemberService;
 
-@Controller
+@RestController
 @RequestMapping("/members")
 public class MemberController {
     
@@ -24,7 +26,7 @@ public class MemberController {
                 .body(memberService.createMember(memberPostPutDTO));
     }
 
-    @PostMapping
+    @GetMapping
     public ResponseEntity<?> getMembers() {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
